e285e86c65369380cc6caedab831f0041b2fb7d8
Ashutosh Raj Singh <ashutosh200210@outlook.com>
2022-09-23 19:46:35 +0530

    impl std::any::Any for all error types

--- src/err.rs
+++ src/err.rs
@@ -1,7 +1,10 @@
+use std::any::Any;
 use std::error::Error;
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
-pub trait ParseError {}
+pub trait ParseError: Debug + Error {
+    fn as_any(&self) -> &dyn Any;
+}
 pub trait TagParseError: Error + ParseError {}
 
 #[derive(Debug)]
@@ -13,21 +16,33 @@ impl Display for UnterminatedStringLiteral {
         write!(f, "Unterminated string literal, found at {}", self.loc)
     }
 }
-impl Error for UnterminatedStringLiteral {} 
-impl ParseError for UnterminatedStringLiteral {}
+impl Error for UnterminatedStringLiteral {}
+impl ParseError for UnterminatedStringLiteral {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 impl TagParseError for UnterminatedStringLiteral {}
 
 #[derive(Debug)]
 pub struct UnterminatedAngularBracket {
-    pub loc: usize
+    pub loc: usize,
 }
 impl Display for UnterminatedAngularBracket {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Unterminated angular bracket, found at location {}", self.loc)
+        write!(
+            f,
+            "Unterminated angular bracket, found at location {}",
+            self.loc
+        )
     }
 }
 impl Error for UnterminatedAngularBracket {}
-impl ParseError for UnterminatedAngularBracket {}
+impl ParseError for UnterminatedAngularBracket {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 
 #[derive(Debug)]
 pub struct PeekOutOfBoundsError {
@@ -37,11 +52,19 @@ pub struct PeekOutOfBoundsError {
 }
 impl Display for PeekOutOfBoundsError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Peek out of bounds, offset {} out of bounds for current index {} and total length {}", self.peek_offset, self.cur_idx, self.len)
+        write!(
+            f,
+            "Peek out of bounds, offset {} out of bounds for current index {} and total length {}",
+            self.peek_offset, self.cur_idx, self.len
+        )
     }
 }
 impl Error for PeekOutOfBoundsError {}
-impl ParseError for PeekOutOfBoundsError {}
+impl ParseError for PeekOutOfBoundsError {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 
 #[derive(Debug)]
 pub struct InvalidFirstTokenError;
@@ -51,22 +74,34 @@ impl Display for InvalidFirstTokenError {
     }
 }
 impl Error for InvalidFirstTokenError {}
-impl ParseError for InvalidFirstTokenError {}
+impl ParseError for InvalidFirstTokenError {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 impl TagParseError for InvalidFirstTokenError {}
 
 #[derive(Debug)]
 pub struct NoTokenAtLocationError {
     pub expected_kind: String,
     pub direction: String,
-    pub current: String
+    pub current: String,
 }
 impl Display for NoTokenAtLocationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Expected {} on the {} of {} token but found nothing", self.expected_kind, self.direction, self.current)
+        write!(
+            f,
+            "Expected {} on the {} of {} token but found nothing",
+            self.expected_kind, self.direction, self.current
+        )
     }
 }
 impl Error for NoTokenAtLocationError {}
-impl ParseError for NoTokenAtLocationError {}
+impl ParseError for NoTokenAtLocationError {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 impl TagParseError for NoTokenAtLocationError {}
 
 #[derive(Debug)]
@@ -77,5 +112,25 @@ impl Display for UnexpectedTagTokenError {
     }
 }
 impl Error for UnexpectedTagTokenError {}
-impl ParseError for UnexpectedTagTokenError {}
+impl ParseError for UnexpectedTagTokenError {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
 impl TagParseError for UnexpectedTagTokenError {}
+
+#[derive(Debug)]
+pub struct TagParseErrorWrapper {
+    pub inner_err: Box<dyn TagParseError>,
+}
+impl Display for TagParseErrorWrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.inner_err)
+    }
+}
+impl Error for TagParseErrorWrapper {}
+impl ParseError for TagParseErrorWrapper {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
