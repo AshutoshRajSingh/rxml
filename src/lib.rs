2d529791f54fc3f099ef54afc7eaf2005a7505d3
Ashutosh Raj Singh <ashutosh200210@outlook.com>
2022-09-27 17:50:11 +0530

    add public api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-pub mod parsedoc;
-pub mod parsetag;
+mod api;
 pub mod error;
-pub mod api;
+mod parsedoc;
+mod parsetag;
+
+use api::XMLNode;
+use error::ParseError;
+use parsedoc::XMLParser;
+use std::rc::Rc;
+
+pub struct RXML {
+    content: String,
+}
+
+impl RXML {
+    pub fn new(content: String) -> Self {
+        Self { content }
+    }
+    pub fn parse(&self) -> Result<Rc<XMLNode>, ParseError> {
+        let parser = XMLParser::new(self.content.as_str());
+        return Ok(parser.parse()?);
+    }
+}
